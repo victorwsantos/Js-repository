5c79217de261eba258c14a0be2e7628901a223e6
victorwsantos <71085593+victorwsantos@users.noreply.github.com>
2021-08-27 11:37:59 -0300

    alterações  estéticas e inclusão do getTime()

--- javaSccript.js
+++ javaSccript.js
@@ -1,4 +1,25 @@
+const hoje = new Date()
+const option = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+}
+
+const horas = hoje.getHours()
+let  msg = ""
+if (horas >= 0 && horas < 12 ){
+    msg = "Bom dia"
+}else if( horas >= 12 && horas <= 18){
+    msg = "Boa tarde"
+}else{
+    msg = "Boa noite"
+}
+console.log(`Hoje é ${hoje.toLocaleDateString('PT-BR', option)}, ${msg}`)
+console.log(`Bem vindo ao meu primiero programa em JavaScript \n`)
+console.log(`Vamos Calcular o valor de parcelas  com e sem Juros?\n`)
 var prod = 500
+console.log(`o valor do porduto é R$${prod},00\n`)
 var cont  = 1
 var parcela = 9
 while (cont <= parcela){
